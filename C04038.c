72691f04ee91c6621cab0483360db224bc1ac0f3
VanTienDesktop <pvantien203@gmail.com>
2023-01-13 21:15:22 +0700

    Sắp xếp chọn - 2

---
+++ C04038.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int a[100];
+int main() {
+	int i, j, k, N, swap, vtMin;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (i = 1; i < N; i++) {
+		vtMin = i;
+		for (j = i + 1; j <= N; j++) {
+			if (a[j] < a[vtMin]) {
+				vtMin = j;
+			}
+		}
+		swap = a[i];
+		a[i] = a[vtMin];
+		a[vtMin] = swap;
+		for (k = 1; k <= N; k++) {
+			printf("%d ", a[k]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
